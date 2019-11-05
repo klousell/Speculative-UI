1a118bd1608aafab72c9c83a737ecda1b34bef1d
Glitch (hello-webpage) <none>
2019-11-05 19:47:57 +0000

    🚓😺 Checkpoint ./css/style.css:8038979/3488 ./js/model-runner.js:8038979/3702 ./index.html:8038979/13109

--- js/model-runner.js
+++ js/model-runner.js
@@ -1,46 +1,45 @@
-let model, webcamEl, predictionCallback
+let model, webcam, predictionCallback;
 
 // This function sets up the model trained in Teachable Machine.
 // it takes in the URL to the model, and a function to be run
 // each time the model makes a new prediction.
-async function setupModel(modelURL, predictionCB) {
+export async function setupModel(URL, predictionCB) {
     //store the prediction callback function
-    predictionCallback = predictionCB
+    predictionCallback = predictionCB;
     // the model.json file stores a reference to the trained model
-    const checkpointURL = `${modelURL}model.json`
+    const modelURL = `${URL}model.json`;
     // the metatadata.json file contains the text labels of your model and additional information
-    const metadataURL = `${modelURL}metadata.json`
+    const metadataURL = `${URL}metadata.json`;
 
     // Load the model using the tmImage library
-    model = await window.tmImage.load(checkpointURL, metadataURL)
+    model = await window.tmImage.load(modelURL, metadataURL);
 
-    // Webcam has a square ratio and is flipped by default to match training
-    const webcamFlipped = true
     // this function from the tmImage library returns a video element that
     // shows a video feed from the webcam
-    webcamEl = await window.tmImage.getWebcam(200, 200, 'front', webcamFlipped)
-    webcamEl.play()
+    webcam = new window.tmImage.Webcam(200, 200, true); //width, height, flipped
+    await webcam.setup(); // request access to the webcam
+    webcam.play();
     // add the video element to the page
-    document.getElementById('webcam-wrapper').appendChild(webcamEl)
+    document.getElementById('webcam-wrapper').appendChild(webcam.canvas);
 
     // kick off the model prediction loop
-    window.requestAnimationFrame(loop)
+    window.requestAnimationFrame(loop);
 }
 
 // This function will run forever in a loop
-async function loop(timestamp) {
+async function loop() {
+    // update the webcam frame
+    webcam.update();
     // make a prediction using the model
-    await predict()
+    await predict();
     // then call loop again
-    window.requestAnimationFrame(loop)
+    window.requestAnimationFrame(loop);
 }
 
 // This function uses the model we loaded to make a prediction on the webcam data
 async function predict() {
     // predict can take in an image, video or canvas html element
-    // we set flip to true since the webcam was only flipped in CSS
-    const flip = true
-    const prediction = await model.predict(webcamEl, flip)
+    const prediction = await model.predict(webcam.canvas);
     // Call the prediction callback function now that we have new prediction data
-    predictionCallback(prediction)
+    predictionCallback(prediction);
 }
